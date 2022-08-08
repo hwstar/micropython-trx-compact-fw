e2cdffde145136f58193abb50a898b3e18d75cf3
hwstar <steve@rodgers619.com>
2022-08-08 15:07:23 -0700

    More work on the menu subsystem

--- lib/display.py
+++ lib/display.py
@@ -4,10 +4,17 @@ import lib.globals as g
 import lib.constants as c
 import lib.gpio_lcd as lcd
 
+
+DISPLAY_LINE_LENGTH = 16
+
+
 #
 # Base class for display
 #
 # This class contains methods which are universal to any display type employed
+#
+# All text strings displayed are stored in this module. This keeps them all in one place.
+# None of the other modules deal with text strings which are displayed on the display.
 #
 #
 
@@ -15,6 +22,7 @@ class _DisplayBase:
     def init(self):
         pass
     
+
     def format_freq(self, freq_hz: int) -> str:
         # Format frequency integer as MM.KKKHHH string
         MHz,kHz = divmod(freq_hz,1000000)
@@ -75,10 +83,22 @@ class Display(_DisplayBase):
         g.event.add_subscriber(self.action, c.ET_DISPLAY)
         self.screens = dict()
         #
-        # Create the "main" virtual screen which is the one used most of the time
+        # Create the "menu" and "main" virtual screens. 
         #
         self.current_screen = "main"
+        self.virt_new_screen("menu")
         self.virt_new_screen(self.current_screen)
+        
+        #
+        # Text for the menu is stored in this
+        # list of lists. The menu module accesses
+        # these strings by group and entry indexes
+        #
+        self.menutext = [
+            ["**Main Menu**",["USB/LSB", "AGC ON/OFF"]], # Group 0
+            ["**LSB/USB**",["LSB", "USB"]], # Group 1
+            ["**AGC**",["ON", "OFF"]] # Group 2
+            ]
         
         
     def virt_switch_screens(self, screen_name):
@@ -93,11 +113,12 @@ class Display(_DisplayBase):
             return
         self.current_screen = screen_name
         # Clear the display
-        g.lcd_clear()
+        g.lcd.clear()
         # Refresh the display with all of the fields which were stored previously
-        for field in self.screen[screen_name]:
-            g.move_to(field["x"], field["y"])
-            g.putstr(field["text"])
+        screen = self.screens[screen_name]
+        for name, field in screen.items():
+            g.lcd.move_to(field["x"], field["y"])
+            g.lcd.putstr(field["text"])
             
     
     def virt_clear_screen(self, screen_name):
@@ -111,6 +132,7 @@ class Display(_DisplayBase):
         #
         if screen_name not in self.screens:
             return
+        # Clear the previous X,Y and Text data
         self.screens[screen_name] = dict()
         # Write through if current screen is the same name
         if self.current_screen == screen_name:
@@ -118,6 +140,7 @@ class Display(_DisplayBase):
             
     def virt_new_screen(self, screen_name):
         # Defines a new screen name
+        #
         # If the screen name does not exist, create it here
         if screen_name not in self.screens:
             self.screens[screen_name] = dict()
@@ -157,6 +180,28 @@ class Display(_DisplayBase):
         elif event_data.subtype == c.EST_DISPLAY_UPDATE_AGC:
             agc_disable = self.format_agc_disable(event_data.data["agc"])
             self.virt_moveto_write(9, 1, agc_disable, "agc")
+        # Main menu entry
+        elif event_data.subtype == c.EST_DISPLAY_MENU_ENTRY:
+            self.virt_switch_screens("menu")
+        elif event_data.subtype == c.EST_DISPLAY_MENU_EXIT:
+            self.virt_switch_screens("main")
+        # Update the menu screen
+        elif event_data.subtype == c.EST_DISPLAY_MENU_UPDATE:
+            mli = event_data.data["group"]
+            mei = event_data.data["entry"]
+            # Pad the menu group so it appears in the center
+            ml_format ="{:^"+"{}".format(DISPLAY_LINE_LENGTH)+"s}"
+            ml_str = ml_format.format(self.menutext[mli][0])
+            # Pad the menu entry so it is left justified
+            me_format ="{:<"+"{}".format(DISPLAY_LINE_LENGTH)+"s}"
+            me_str = me_format.format(self.menutext[mli][1][mei])
+            self.virt_moveto_write(0, 0, ml_str, "group", "menu")
+            self.virt_moveto_write(0, 1, me_str, "entry", "menu")
+            
+            
+            
+            
+            
             
         
         
--- lib/menu.py
+++ lib/menu.py
@@ -1,22 +1,48 @@
 import micropython
 import lib.globals as g
 import lib.constants as c
+import lib.event as ev
 
+# Note that all menu text is handled by the display module
 
 class Menu:
     def init(self):
         self.in_menu_system = False
         g.event.add_subscriber(self.action, c.ET_ENCODER|c.ET_SWITCHES)
-        pass
+        self.group = 0
+        self.entry = 0
+    
+       
     
     def active(self):
         return self.in_menu_system
     
     def action(self, event_data):
+      
         # Knob switch long press enters/exits menu system
         if event_data.subtype == c.EST_KNOB_RELEASED_LONG:
             self.in_menu_system = not self.in_menu_system
-            print("In menu system: ", self.in_menu_system)
+            #print("In menu system: ", self.in_menu_system)
+            if self.in_menu_system:
+                # Write menu text
+                self.group = 0
+                self.entry = 0
+            
+                ed = ev.EventData(c.ET_DISPLAY, c.EST_DISPLAY_MENU_UPDATE, {"group": self.group, "entry": self.entry})
+                g.event.publish(ed)
+                # Display menu text
+                ed = ev.EventData(c.ET_DISPLAY, c.EST_DISPLAY_MENU_ENTRY)
+                g.event.publish(ed)
+            else:
+                # Switch back to normal operation
+                ed = ev.EventData(c.ET_DISPLAY, c.EST_DISPLAY_MENU_EXIT)
+                g.event.publish(ed)
+                
+                
+                
+            
+                
+            
       
     
     
